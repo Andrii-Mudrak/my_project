e99c995c3775c9fa1ec266b9f5e5b52b83268aa2
andriymudrak@gmail.com <andriymudrak@gmail.com>
2020-10-19 00:02:25 +0300

    Змінено models.py: 1. додав зовнішній ключ на таблицю 1-до-1; 2. змінено аргументи CharField та інших; 3. додав валідатори для телефона; 4. замінив назву з User на Profile.
    Додано базу в gitignore.
    
    Змінено версію Django на актуальну.
    
    Змінено відключення до БД на dj-database-url

--- mainapp/models.py
+++ mainapp/models.py
@@ -1,47 +1,63 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import MinLengthValidator, int_list_validator
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 class Comments(models.Model):
-    id = models.AutoField(primary_key=True)
-    content = models.TextField(max_length=400)
-    created_at = models.DateTimeField()
-    deleted_at = models.DateTimeField()
-    user_id = models.ForeignKey('User', on_delete=models.CASCADE)
-    product_id = models.ForeignKey('Product', on_delete=models.CASCADE)
+    id = models.BigAutoField(primary_key=True)
+    content = models.TextField(max_length=400, blank=False)
+    created_at = models.DateTimeField(default=datetime.utcnow())
+    deleted_at = models.DateTimeField(null=True)
+    user_id = models.ForeignKey('user_id', on_delete=models.SET_NULL, null=True)
+    product_id = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.content
 
 class Product(models.Model):
-    id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=100)
-    content = models.TextField(max_length=400)
-    created_at = models.DateTimeField()
-    deleted_at = models.DateTimeField()
-    is_active = models.BooleanField()
-    autor_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    id = models.BigAutoField(primary_key=True)
+    title = models.CharField(max_length=100, blank=False)
+    content = models.TextField(max_length=400, blank=False)
+    created_at = models.DateTimeField(default=datetime.utcnow())
+    deleted_at = models.DateTimeField(null=True)
+    is_active = models.BooleanField(default=False)
+    autor_id = models.ForeignKey('user_id', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.content
 
 class Responces(models.Model):
-    id = models.AutoField(primary_key=True)
-    message = models.CharField(max_length=100)
-    created_at = models.DateTimeField()
-    deleted_at = models.DateTimeField()
-    product_id = models.ForeignKey('Product', on_delete=models.CASCADE)
-    autor_id = models.ForeignKey('User', on_delete=models.CASCADE)
+    id = models.BigAutoField(primary_key=True)
+    message = models.CharField(max_length=100, blank=False)
+    created_at = models.DateTimeField(default=datetime.utcnow())
+    deleted_at = models.DateTimeField(null=True)
+    product_id = models.ForeignKey('Product', on_delete=models.SET_NULL, null=True)
+    autor_id = models.ForeignKey('user_id', on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.message
 
+class Profile(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    name = models.CharField(max_length=100, blank=False)
+    phone = models.CharField(max_length=12, blank=False,
+                             validators=[int_list_validator(sep=''),
+                                         MinLengthValidator(12),],
+                             default='380671234567')
+    created_at = models.DateTimeField(default=datetime.utcnow())
+    updated_at = models.DateTimeField(default=datetime.utcnow(), on_update=datetime.utcnow())
+    deleted_at = models.DateTimeField(null=True)
+    is_verified = models.BooleanField(default=False)
+    user_id = models.ForeignKey('User', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class User(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=100)
-    phone = models.CharField(max_length=100)
-    created_at = models.DateTimeField()
-    updated_at = models.DateTimeField()
-    deleted_at = models.DateTimeField()
-    is_verified = models.BooleanField()
-    'тут потрібно 1 до 1 юзера вклеїть'
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+
+    def __str__(self):
+        return self.user
--- my_project/settings.py
+++ my_project/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
-
+import dj_database_url
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -74,13 +74,13 @@ WSGI_APPLICATION = 'my_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
-}
-
+}'''
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
